ebc1c3694d9ec808f67b92cbc17c7354932ddd03
nasimBigdelu <nasim.bigdelu@gmail.com>
2016-08-01 13:09:47 -0700

    Add project files.

---
+++ Trackify/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Trackify.Startup))]
+namespace Trackify
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
