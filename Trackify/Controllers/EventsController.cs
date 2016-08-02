e995d9f56151a49edce4e23408d6d786cf35809d
nasimBigdelu <nasim.bigdelu@gmail.com>
2016-08-02 11:44:38 -0700

    Created Add an Event Form

---
+++ Trackify/Controllers/EventsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Trackify.Models;
+using Trackify.ViewModels;
+
+namespace Trackify.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new EventFormViewModel
+            {
+                Categories = _context.Categories.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Trackify/ViewModels/EventFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Trackify.Models;
+
+namespace Trackify.ViewModels
+{
+    public class EventFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
