96a17322c43a9fbfad72a05d9fa4ba4306d63aee
nasimBigdelu <nasim.bigdelu@gmail.com>
2016-08-12 16:27:36 -0700

    Implemented Anti Forgery Token

--- Trackify/Controllers/EventsController.cs
+++ Trackify/Controllers/EventsController.cs
@@ -28,6 +28,7 @@ namespace Trackify.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(EventFormViewModel viewModel)
         {
             if (!ModelState.IsValid)
